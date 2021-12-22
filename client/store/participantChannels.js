1ca1520193159e881b0dd869324cba3f3e9d7c0b
Anatoly Tsinker <anatoly.tsiner12@gmail.com>
2021-12-22 15:51:19 -0500

    refactored flattened and commented code from redux store chat.js to participantChannels.js

--- client/store/participantChannels.js
+++ client/store/participantChannels.js
@@ -3,9 +3,41 @@ import socket from "../socket";
 import { authenticateRequest } from "./gateKeepingMiddleWare";
 
 //  Action Declarations
+
+const GET_PARTICIPANT_CHANNELS = "GET_PARTICIPANT_CHANNELS";
+const CLEAR_PARTICIPANT_CHANNELS = "CLEAR_PARTICIPANT_CHANNELS"
+
 //  Action Creators
 
+const getParticipantChannels = (channels) => {
+  return {
+    type: GET_PARTICIPANT_CHANNELS,
+    channels,
+  };
+};
+
+//  no thunk for this action. Used to clear local state only.
+export const clearParticipantChannels = () => {
+  return {
+    type: CLEAR_PARTICIPANT_CHANNELS,
+  }
+}
+
 //  Thunks / async calls
+
+//  fetch channels that the used has posted at least one message in
+//    from our database.
+export const fetchParticipantChannels = () => async (dispatch) => {
+  try {
+    const data = await authenticateRequest(
+      "get",
+      "api/messages/channels/participant"
+    );
+    dispatch(getParticipantChannels(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 //  I.State;
 
@@ -15,7 +47,10 @@ const initialState = []
 
 export default (state = initialState, action) => {
   switch (action.type) {
-
+    case GET_PARTICIPANT_CHANNELS:
+      return action.channels;
+    case CLEAR_PARTICIPANT_CHANNELS:
+      return initialState;
     default:
       return state
   }
